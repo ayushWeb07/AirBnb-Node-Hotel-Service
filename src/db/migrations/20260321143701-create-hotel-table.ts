f461bfa7e47631eac8575577b6d3400dd1d4e646
Ayush Bommana <bommanaayush07@gmail.com>
2026-03-21 22:29:15 +0530

    feat: Integrate MySQL with Sequelize ORM

---
+++ src/db/migrations/20260321143701-create-hotel-table.ts
@@ -0,0 +1,18 @@
+import { DataTypes, type QueryInterface } from "sequelize";
+
+export default {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.createTable("hotels", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+    });
+  },
+
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.dropTable("hotels")
+  },
+};
