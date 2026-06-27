0be39f0053e65ae06b7400ffe239623f3a161ad1
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-28 03:58:55 +0530

    feat: Use producer inside controller

--- src/controllers/roomGeneration.controller.ts
+++ src/controllers/roomGeneration.controller.ts
@@ -1,16 +1,14 @@
 import type { Request, Response } from "express";
-import * as roomGenerationService from "../services/roomGeneration.service.ts";
 
 import { StatusCodes } from "http-status-codes";
+import {addJobToRoomGenerationQueue} from "../producers/roomGeneration.producer.ts";
 
 const createRoomGenerationJob = async (req: Request, res: Response) => {
-	const roomGenerationJob = await roomGenerationService.createRoomGenerationJob(
-		req.body,
-	);
+	// enqueue the job into the queue
+	await addJobToRoomGenerationQueue(req.body)
 
 	res.status(StatusCodes.CREATED).json({
 		message: "New rooms were created successfully",
-		data: roomGenerationJob,
 		success: true,
 	});
 };
