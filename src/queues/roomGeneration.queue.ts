f74020111f164718aaff56b12f3d25e234f63612
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-28 13:03:25 +0530

    fix: Remove unneccessary type casting in queues

--- src/queues/roomGeneration.queue.ts
+++ src/queues/roomGeneration.queue.ts
@@ -1,12 +1,11 @@
 import { Queue } from "bullmq";
 import { RedisConnection } from "../config/redis.config.ts";
 import { serverConfig } from "../config/index.ts";
-import { Redis } from "ioredis";
 
 const roomGenerationQueue = new Queue(
 	serverConfig.BULLMQ_ROOM_GENERATION_QUEUE_NAME,
 	{
-		connection: RedisConnection.getConnectionObject() as Redis,
+		connection: RedisConnection.getConnectionObject(),
 	},
 );
 
